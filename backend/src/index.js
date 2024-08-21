b8b07b05f043913f33e6174ea2bbcc7cefe2c7b4
Mjdiazri <citruss2012@hotmail.es>
2024-08-20 22:45:40 -0500

    Se crea y ajusta package.json e index.js. Se crean carpetas dentro de backend (config, controller, models, routes

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+//Importar Express
+import express from 'express';
+
+//Importar Cors
+import cors from 'cors';
+
+//Importar conexion a base de datos (Previamente se debe crear archivo db.js en carpeta config)
+import conectarDb from '../config/db.js';
